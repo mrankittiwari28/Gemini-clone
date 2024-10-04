0c66d06ec1c68764bffbe33bbb115f7aa2887b27
Ankit <ankittiwari285202@gmail.com>
2024-10-04 19:56:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Sidebar from './sidebar/Sidebar'
+import Main from './main/Main'
+
+
+
+const App = () => {
+  return (
+    <>
+
+    <Sidebar/>
+    <Main/>
+      
+    </>
+  
+  )
+}
+
+export default App
